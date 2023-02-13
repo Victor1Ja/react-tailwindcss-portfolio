7d59c2ef2c6dacc44bc44689815622f088c3348d
Victor Javier López Roque <victor.98.javier@gmail.com>
2023-02-12 20:23:58 -0500

    Refactor NavBar component Move hardcoded string to the data file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
-import React from "react";
+import { ArrowRightIcon } from '@heroicons/react/solid';
+import React from 'react';
+import { navBar } from '../data';
 
 export default function Navbar() {
   return (
@@ -7,24 +8,20 @@ export default function Navbar() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+            {navBar.title}
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
-          </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-            Skills
-          </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
-          </a>
+          {navBar.tabs.map((tab, index) => (
+            <a href={tab.value} key={index} className="mr-5 hover:text-white">
+              {tab.text}
+            </a>
+          ))}
         </nav>
         <a
-          href="#contact"
+          href={navBar.finalTab.value}
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Hire Me
+          {navBar.finalTab.text}
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
